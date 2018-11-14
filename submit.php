ef03444c703e47b88e1fd0aed619a41ef14a3d13
Juan García <39386985+ElTroesma@users.noreply.github.com>
2018-11-14 13:35:51 -0300

    Creación del PHP Mailer

---
+++ submit.php
@@ -0,0 +1,38 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="assets/css/master.css">
+		<link rel="stylesheet" href="assets/css/submit.css">
+		<link rel="icon" type="image/png" href="assets/media/favicon.png">
+		<link href='http://fonts.googleapis.com/css?family=Asap:400,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=divice-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<title>Gestion Arte</title>
+	</head>
+	<body>
+
+<?php
+	require 'PHPMailer/PHPMailerAutoload.php';
+
+	$mail = new PHPMailer;
+
+	$to = "gestionarteadm@gmail.com";
+
+	$nombre = $_POST['firstname'];
+	$apellido = $_POST['lastname'];
+	$telefono = $_POST['phone'];
+	$email = $_POST['email'];
+	$mensaje = nl2br($_POST['message']);
+
+		echo '<br><div class="mensaje container alert alert-success" id="return-submit"><span class="submit-span">Su mensaje se ha enviado con éxito. </span><a href="index.html" class="submit-a">Pulse aquí</a><span class="submit-span"> para volver.</span></div>';
+		$mail->From = $email;
+		$mail->addAddress($to);
+		$mail->isHtml(true);
+		$mail->Body = '<strong>Nombre:</strong>  '.$nombre.'<br><br><strong>Apellido:</strong>  '.$apellido.'<br><br><strong>Correo de contacto:</strong>  '.$email.'<br><br><strong>Teléfono:</strong>  '.$telefono.'<br><br><strong>Nos a enviado el siguiente mensaje:<br><br></strong><p>'.$mensaje.'</p>';
+		$mail->CharSet = 'UTF-8';
+		$mail->send();
+
+?>
+	</body>
+
+</html>
