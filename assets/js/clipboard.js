88d2f58ca4f0f934cb05b3ccc7c9b55b8f2f92ae
Juan García <juan.garcia070699@outlook.es>
2018-11-16 00:52:34 -0300

    Avance con la pestaña de inmuebles

---
+++ assets/js/clipboard.js
@@ -0,0 +1,8 @@
+function getlink() {
+  var aux = document.createElement("input");
+  aux.setAttribute("value",window.location.href);
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand("copy");
+  document.body.removeChild(aux);
+}
